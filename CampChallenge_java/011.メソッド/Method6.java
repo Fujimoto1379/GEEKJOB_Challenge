11894772f8a7efdccfe520c18fdd8390d577d7b3
TUNK\Ryo <fujimoto1379@gmail.com>
2018-10-10 20:07:39 +0900

    引数、戻り値１

---
+++ CampChallenge_java/011.メソッド/Method6.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hikisuu.newpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Ryo
+ */
+@WebServlet(name = "Method6", urlPatterns = {"/Method6"})
+public class Method6 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+    //順番に各配列の0番目の値と引数から受け取った数値型の値で比較処理。
+    //比較して一致した場合、呼び出し元へ一致した配列を返却。
+   String[] userProfile(int num){
+    String [][] userProfile_list={
+        {"1","技育太郎","東京","男","プログラマー"},
+        {"2","技育花子","北海道","女","システムエンジニア"},
+        {"3","技育三郎","大阪","男","Webエンジニア"}
+        
+    };
+       int i;
+    for(i = 0; i <= 3; i++){
+    int intValue = Integer.parseInt(userProfile_list[i][0]);
+        if(num == intValue){
+            //どの配列を戻したいか指定する。
+        return userProfile_list[i];
+    }
+    }
+       return null;
+   } 
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+            //引数が指定されている。
+            int num = 1 ;
+           for (int i = 1; i < userProfile(num).length; i++){
+               out.print(userProfile(num)[i]);
+        }
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
